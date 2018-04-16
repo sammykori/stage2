38c5659610f16e17de0a7ff9b13438bd0633c99a
sammykori <sammykori72@gmail.com>
2018-04-16 14:36:12 +0100

    Initial Commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+
+    </head>
+    <body style="background-image: url(time.jpg); background-size: cover;">
+        <div style="width: 100%; height: 200px; background-color: rgba(255,0,0,0.3); margin-top: 200px;">
+            <h1 style="color:white; text-align: center; padding-top: 20px; font-family:'digital-7';
+            src: url(digital-7.ttf); font-size: 90px"><?php echo date("h:i:sa"); ?></h1>
+
+            <p style="color:white; text-align: center; padding-top: 15px; font-family:'digital-7';
+            src: url(digital-7.ttf); font-size: 10px">HNG INTERNSHIP STAGE 1</p>
+        </div>
+    </body>
+</html>
